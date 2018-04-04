a3fb588d1bdc817c4b7865deb727ed123ca84b3d
ez <ez@ezdebian.ezdebian.me>
2018-04-04 19:28:26 +0800

    Add a header for common defination.

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef _COMMON_H_
+#	define _COMMON_H_
+
+typedef int rtn_status;
+
+#	define STAT_SUCC  0
+// #	define STAT_FAIL  1
+#	define STAT_ERR   -2
+
+#	define CMP_GT  -1
+#	define CMP_EQ   0
+#	define CMP_LT   1
+
+#endif // ~ _COMMON_H_
