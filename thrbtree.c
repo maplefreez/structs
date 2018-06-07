132c4fe3d6c89bd14e290acf58f164033f208b8f
maplefreez <maplefreez@163.com>
2018-06-07 21:22:03 +0800

    Add a global variable for threaded binary search function.

--- thrbtree.c
+++ thrbtree.c
@@ -2,6 +2,9 @@
 #include "common.h"
 #include "thrbtree.h"
 
+
+/* For function _thrbt_inorderthread. */
+static thrpbtree __prenode_threading;
 static void _thrbt_inorderthread (pthrbtree, pthrnode);
 
 
@@ -14,17 +17,35 @@ thrpbtree thrbt_create_pre_inorder (char* _preo, char* _ino) {
 
 thrpbtree thrbt_create_post_inorder (char*, char*);
 
+/* TODO TEST... */
 void thrbt_inorderthreaded (pthrbtree _t) {
+	if (! _t) return;
+
+	/* Firstly we set the global 
+	 * variable to root node. */
+	__prenode_threading = _t;
+	_thrbt_inorderthread (_t /* , __prenode_threading */);
+
+	/* The last node. */
+	if (__prenode_threading && 
+			(! __prenode_threading -> right || 
+			__prenode_threading == _t)) {
+		__prenode_threading -> rflag = THREAD;
+		__prenode_threading -> right = NULL;
+	}
+
+	/* Reset global variable. */
+	__prenode_threading = NULL;
 }
 
 
 /* Note: the last leaf a tree shell be
  * marked by the function caller. */
 static pthrbtree _thrbt_inorderthread (
-		pthrbtree _t, pthrnode _prenode) {
+		pthrbtree _t /* , pthrnode _prenode */) {
 	if (_t) {
 		/* left child tree. */
-		_thrbt_inorderthread (_t -> left, _t);
+		_thrbt_inorderthread (_t -> left);
 
 		/* Current node. We just mark it's 
 		 * immediate predecessor, and the 
@@ -32,22 +53,25 @@ static pthrbtree _thrbt_inorderthread (
 		 * recursive calling. */
 		if (! _t -> left) {
 			_t -> ltag = THREAD;
-			_t -> left = _prenode;
+			_t -> left = __prenode_threading;
 		}
 
 		/* It's predecessor. */
-		if (! _prenode -> right) {
-			_prenode -> rflag = THREAD; 
-			_prenode -> right = _t;
+		if (__prenode_threading && 
+				! __prenode_threading -> right) {
+			__prenode_threading -> rflag = THREAD; 
+			__prenode_threading -> right = _t;
 		}
 
-		_thrbt_inorderthread (_t -> right, _t);
+		__prenode_threading = _t;
+
 		/* right child tree. */
+		_thrbt_inorderthread (_t -> right);
 	}
 
-	/* TODO: NOTE the _prenode parameter. */
 	return _t;
 }
+
 
 void thrbt_pretraversal (pthrbtree, bt_visitf);
 void thrbt_posttraversal (pthrbtree, bt_visitf);
