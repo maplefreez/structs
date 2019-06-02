502fd62930abd094bcf1337bd66364f11f456c2d
maplefreez <maplefreez@163.com>
2019-06-02 09:41:11 +0800

    Add test suits for isempty_cqueue(), count_cqueue(), isfull_cqueue ();

--- t_queue.c
+++ t_queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <assert.h>
 #include "queue.h"
 
@@ -35,13 +36,43 @@ static void t_cqueue_new () {
 
 
 static void t_cqueue_isempty () {
-	int res = 0;
-	pcqueue queue = new_cqueue ();
-	assert (queue);
+	{ // 0x01
+		int res = 0;
+		pcqueue queue = new_cqueue ();
+		assert (queue);
 
-	res = isempty_cqueue (queue);
-	assert (res == 1);
-	// TODO...
+		res = isempty_cqueue (queue);
+		assert (res == 1);
+		release_cqueue (queue);
+	}
+
+	{ // 0x02
+		int res = 0, i;
+		pcqueue queue = new_cqueue ();
+		assert (queue);
+
+		res = isempty_cqueue (queue);
+		assert (res == 1);
+		
+		/* Enqueue 15 elements. */
+		for (i = 1; i < __DEF_QUEUE_INIT_LEN; ++ i) 
+			{
+				assert (enqueue_cqueue (queue, (anytype) i));
+				assert (! isempty_cqueue (queue));
+			}
+
+		/* Dequeue 15 elements. */
+		i ^= i;
+		while (++ i < __DEF_QUEUE_INIT_LEN - 1) {
+			assert (dequeue_cqueue (queue));
+			assert (! isempty_cqueue (queue));
+		}
+
+		assert (dequeue_cqueue (queue));
+		assert (isempty_cqueue (queue));
+
+		release_cqueue (queue);
+	}
 }
 
 static void t_cqueue_isfull () {
@@ -55,7 +86,7 @@ static void t_cqueue_isfull () {
 		release_cqueue (queue);
 	}
 
-	{
+	{ // 0x02
 		int x = 1; int count; 
 		pcqueue q = new_cqueue ();
 		assert (q);
@@ -72,16 +103,64 @@ static void t_cqueue_isfull () {
 		release_cqueue (q);
 	}
 
+	{ // 0x03 
+		int x = 1;
+		pcqueue q = new_cqueue ();
+		assert (q);
+
+		for (; x <= 14; ++ x) {
+			assert (enqueue_cqueue (q, (anytype) x));
+			assert (! isfull_cqueue (q));
+		}
+		assert (enqueue_cqueue (q, (anytype) x));
+		assert (isfull_cqueue (q));
+
+		/* Dequeue 9 elements. */
+		while (x ++ <= 23) {
+			assert (dequeue_cqueue (q));
+			assert (! isfull_cqueue (q));
+		}
+
+		/* Enqueue 9 elements. */
+		while (x ++ <= 32) {
+			assert (enqueue_cqueue (q, (anytype) x));
+			assert (! isfull_cqueue (q));
+		}
+
+		assert (enqueue_cqueue (q, (anytype) x));
+		assert (isfull_cqueue (q));
+
+		release_cqueue (q);
+	}
+
 }
 
 static void t_cqueue_count () {
-	int res = 0;
-	pcqueue queue = new_cqueue ();
-	assert (queue);
+	{ // 0x01
+		int i, flag, c = 0;
+		int num = 0;
+		pcqueue q = new_cqueue ();
+		assert (q);
 
-	res = count_cqueue (queue);
-	assert (res == 0);
-	// TODO...
+		assert (0 == count_cqueue (q));
+
+		srand (time (NULL));
+		for (i = 1; i <= 4096; ++ i) {
+			num = rand ();
+			srand (num);
+			flag = num >= RAND_MAX / 2;
+
+			if (flag) {
+				c += !! (flag && !! enqueue_cqueue (q, (anytype) i));
+				assert (c == count_cqueue (q));
+			} else {
+				c -= !! dequeue_cqueue (q);
+				assert (c == count_cqueue (q));
+			}
+		}
+
+		release_cqueue (q);
+	}
 }
 
 
