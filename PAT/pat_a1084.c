575046b01b3665a54bb1aabcc945de9ef74e6f44
maplefreez <maplefreez@163.com>
2019-09-07 21:01:35 +0800

    Add PAT A 1084 problem.

--- PAT/pat_a1084.c
+++ PAT/pat_a1084.c
@@ -3,7 +3,40 @@
  * Desc :  PAT A 1084
  */
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include <stdbool.h>
+
+static bool has_output [128] = {false, };
+static char right_string [96], err_string [96];
+
+#	define __to_capa(__C, __v)     \
+	do {											\
+		if (__C >= 'a' && __C <= 'z') __v = __C - 32;	\
+		else __v = __C;  \
+	} while (0)
+
+int main (int  argc, char* argv []) {
+	int i = 0, j = 0; size_t right_len, err_len;
+	if (scanf ("%s\n%s", right_string, err_string));
+
+	right_len = strlen (right_string);
+	err_len = strlen (err_string);
+
+	while (i < right_len) {
+		char r, e;
+		__to_capa (right_string [i], r); j = 0;
+		while (j < err_len) {
+			__to_capa (err_string [j], e);
+			if (e == r) break;
+			else j ++;
+		}
+		if (j == err_len && ! has_output [(int) r]) {
+			putchar (r); has_output [(int) r] = true;
+		}
+		i ++;
+	}
+
+	return 0;
+}
+
