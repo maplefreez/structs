28c70fb0b47e196f30197659b0fb6b7bccf76081
maplefreez <maplefreez@163.com>
2019-12-30 16:51:58 +0800

    PAT problem B 1007. To be submitted.

---
+++ PAT/pat_b1007.c
@@ -0,0 +1,33 @@
+/* Date : 2019/12/26
+ * Author : ez
+ * Desc :  PAT A 1081
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+static int _is_prime (int);
+
+/* TODO... To be submitted. */
+int main (int argc, char * argv []) {
+	int n, i = 3, count = 0;
+	if (scanf ("%d", &n));
+	
+	for (; i + 2 <= n; i += 2) {
+		if (_is_prime (i) && _is_prime (i + 2)) 
+			count ++;
+	}
+	printf ("%d", count);
+	return 0;
+}
+
+
+static int _is_prime (int x) {
+	int i = 2, right = (int) sqrt (x);
+	for (; i <= right; ++ i)
+		if (x % i == 0) return 0;
+	return 1;
+}
+
